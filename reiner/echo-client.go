05bf5ee3078a82c6c7b0573548a5a93713d479bf
BradHacker <harkerbd@gmail.com>
2021-02-11 16:35:11 -0500

    Setup basic client-server socket test. Closes #5

---
+++ reiner/echo-client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	connHost = "localHost"
+	connPort = "1337"
+	connType = "tcp"
+)
+
+func main() {
+	conn, err := net.Dial(connType, connHost+":"+connPort)
+
+	if err != nil {
+		fmt.Println("Error connecting:", err.Error())
+		os.Exit(1)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Message: ")
+
+		input, _ := reader.ReadBytes('\n')
+
+		conn.Write(input)
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+		log.Print("Server echo: ", message)
+	}
+}
